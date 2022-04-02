3de5439a21dad9c4d782f622882859e4a9b289b5
Justin O'Neill <justinoneill2007@gmail.com>
2022-04-02 06:44:07 -0500

    made scrollbar more awesome

--- src/lib/Scrollbar.svelte
+++ src/lib/Scrollbar.svelte
@@ -3,6 +3,7 @@
 </script>
 
 <Svrollbar />
+<!-- <Svrollbar alwaysVisible={true} /> -->
 
 <style>
   :global(html),
@@ -31,27 +32,41 @@
     --svrollbar-thumb-shadow:	initial;
     --svrollbar-thumb-opacity: 1;
     --svrollbar-thumb-background: black;
-    --svrollbar-thumb-background: 
+    /* --svrollbar-thumb-background:  
       linear-gradient( -60deg, 
-        rgb(0, 255, 0), 
-        rgb(255, 255, 0), 
-        rgb(255, 0, 0)
-      );
+        rgba(0, 0, 0, 1),
+        rgba(0, 0, 0, .15) 40%, 
+        rgba(0, 0, 0, .15) 60%, 
+        rgba(0, 0, 0, 1)
+      ); */
   }
+
+  :global(html .v-scrollbar) { transform: scale(.98); z-index: 20; }
+  /* :global(html .v-thumb) { 
+    border: 1px solid black;
+    backdrop-filter: blur(20px) brightness(100%);
+    -webkit-backdrop-filter: blur(20px) brightness(100%); 
+  } */
+  
+  /* :global(html .v-track)    {  } */
+  /* :global(html .v-thumb)    {  } */
 
   :global(html.dark) {
 
     /* Thumb */
-    --svrollbar-thumb-background: white;
-    --svrollbar-thumb-background: 
+    /* --svrollbar-thumb-background:  rgba(255,255,255,.5); */
+    --svrollbar-thumb-background:  
       linear-gradient( -60deg, 
-        rgb(0, 0, 255),
-        rgb(255, 0, 255), 
-        rgb(255, 0, 0)
+        rgba(255, 255, 255, 1),
+        rgba(255, 255, 255, .65) 40%, 
+        rgba(255, 255, 255, .65) 60%, 
+        rgba(255, 255, 255, 1)
       );
   }
 
-  :global(.v-scrollbar)  { transform: scale(.98); }
-  /* :global(.v-track)      {  } */
-  /* :global(.v-thumb)      {  } */
+  :global(html.dark .v-thumb) { 
+    border: 1px solid white;
+    backdrop-filter: blur(20px) brightness(1500%);
+    -webkit-backdrop-filter: blur(20px) brightness(1500%); 
+  }
 </style>
